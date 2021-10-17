c2715aedc53ff023901d96a6b5ab4b141e9fa3eb
Capta1n-n9m0 <alievabbas1@gmail.com>
2021-10-17 22:26:56 +0400

    Capture data from ICE.txt

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
-# This is a sample Python script.
+from pprint import pprint
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def main():
+    data = list()
+    with open("ICE.txt", 'r') as ICE:
+        for line in ICE.readlines():
+            line_data = line.split("   ")
+            line_data.pop(0)
+            line_data[12] = line_data[12][:-1]
+            data.append(line_data)
+    for ii in data:
+        print(ii)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
